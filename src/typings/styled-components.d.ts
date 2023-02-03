9a52b25488eb337191e72f0e84396099f4d051ac
Hugo Fabricio <me@hugofabricio.com>
2023-02-02 21:53:26 -0300

    Global and styled typings

--- src/typings/styled-components.d.ts
+++ src/typings/styled-components.d.ts
@@ -2,6 +2,11 @@ import { theme } from 'styles'
 
 declare module 'styled-components' {
   type Theme = typeof theme
+  type Breakpoint = keyof Theme['grid']['breakpoints']
+  type Color = keyof Theme['colors']
+  type FontSize = keyof Theme['typography']['sizes']
+  type FieldSize = keyof Theme['form']['input']
+  type ButtonSize = keyof Theme['button']
 
   export interface DefaultTheme extends Theme {}
 }

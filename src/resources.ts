15f133644b09d8f7bf2dda814bd40c2039f66d73
Hugo Fabricio <me@hugofabricio.com>
2023-02-02 21:54:43 -0300

    Init create slices components

--- src/resources.ts
+++ src/resources.ts
@@ -1,3 +1,44 @@
+export type HeroSlice = {
+  title: string
+  description: string
+  ctaButton: string
+  backgroundImage: string
+  belowLinks: string
+  links: {
+    image: string
+    url: string
+  }[]
+}
+
+export type HeadlineSlice = {
+  title: string
+  subtitle: string
+  description?: string
+  image: string
+}
+
+export type FeatureSlice = {
+  ctaHighlightText: string
+  ctaRegularText: string
+  description: string
+  button: string
+  image: string
+  imagePosition: 'left' | 'right'
+  backgroundImage: string
+}
+
+export type Slice =
+  | { name: 'hero'; data: HeroSlice }
+  | { name: 'simple_horizontal' | 'simple_headline'; data: HeadlineSlice }
+  | { name: 'stacked_list'; items: FeatureSlice[] }
+
+const SLICES: Record<SliceName, Slice> = {
+  hero: slices.Hero,
+  simple_horizontal: slices.Headline,
+  simple_headline: slices.Headline,
+  stacked_list: slices.Feature
+}
+
 export type Page = {
   slug: string
   locale: string
@@ -11,11 +52,7 @@ export type Page = {
       item: string
     }[]
   }[]
-  contentSections: {
-    name: string
-    data?: Record<string, unknown>
-    items?: Record<string, unknown>
-  }[]
+  contentSections: Slice[]
   footer: {
     sections: {
       name: string
@@ -23,6 +60,6 @@ export type Page = {
         label: string
         url: string
       }[]
-    }
+    }[]
   }
 }

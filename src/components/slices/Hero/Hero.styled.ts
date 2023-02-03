9ff41967cb33da86977a51047fdb92e1a7549329
Hugo Fabricio <me@hugofabricio.com>
2023-02-03 14:12:06 -0300

    Continue hero slice

--- src/components/slices/Hero/Hero.styled.ts
+++ src/components/slices/Hero/Hero.styled.ts
@@ -1,4 +1,20 @@
 import styled from 'styled-components'
+import { media } from 'utils'
+
+export const ImageLink = styled.a``
+
+export const List = styled.div`
+  display: flex;
+  gap: 12px;
+`
+
+export const Links = styled.div`
+  margin-top: 60px;
+
+  ${media.lessThan('sm')} {
+    margin-top: 20px;
+  }
+`
 
 export const Background = styled.div`
   position: absolute;
@@ -8,16 +24,36 @@ export const Background = styled.div`
   width: 100%;
   height: 100%;
   user-select: none;
+
+  ${media.lessThan('sm')} {
+    position: relative;
+    height: 350px;
+  }
 `
 
 export const Caption = styled.div`
+  padding-top: 60px;
   max-width: 500px;
+
+  ${media.lessThan('sm')} {
+    text-align: center;
+
+    .cta {
+      display: none;
+    }
+  }
 `
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.section`
   position: relative;
   height: 100vh;
   min-height: 700px;
   display: flex;
   align-items: center;
+
+  ${media.lessThan('sm')} {
+    flex-direction: column;
+    height: auto;
+    min-height: auto;
+  }
 `
--- src/components/slices/Hero/Hero.tsx
+++ src/components/slices/Hero/Hero.tsx
@@ -9,26 +9,12 @@ interface HeroProps {
   data: HeroSlice
 }
 
-//   "data": {
-//     "ctaButton": "Comece já",
-//     "belowLinks": "EXPERIMENTE GRÁTIS",
-//     "links": [
-//       {
-//         "image": "https://storage.googleapis.com/moises-cms/app_store_3279bb50bd/app_store_3279bb50bd.svg",
-//         "url": "https://apps.apple.com/app/apple-store/id1515796612?pt=121597498&ct=home_hero&mt=8"
-//       },
-//       {
-//         "image": "https://storage.googleapis.com/moises-cms/app_google_play_1_3cdc01c8aa/app_google_play_1_3cdc01c8aa.svg",
-//         "url": "https://play.google.com/store/apps/details?id=ai.moises&referrer=utm_source%3Dhome_hero%26utm_medium%3Dwww%26utm_campaign%3Dhome_hero"
-//       }
-//     ]
-
 const Hero = ({ data }: HeroProps) => {
   return (
     <S.Wrapper>
       <Container>
         <S.Caption>
-          <Text fontSize="h1" weight={600}>
+          <Text tag="h1" fontSize="h1" weight={600}>
             {data.title}
           </Text>
           <Text
@@ -40,9 +26,27 @@ const Hero = ({ data }: HeroProps) => {
           >
             {data.description}
           </Text>
-          <Button href="#" title={data.ctaButton} fieldSize="lg" marginTop={30}>
+          <Button
+            href="#"
+            className="cta"
+            title={data.ctaButton}
+            fieldSize="lg"
+            marginTop={30}
+          >
             {data.ctaButton}
           </Button>
+          <S.Links>
+            <Text weight={500} letterSpacing={0.5} marginBottom={15}>
+              {data.belowLinks}
+            </Text>
+            <S.List>
+              {data.links.map(({ image, url }, i) => (
+                <S.ImageLink key={`link-${i}`} title="Download" href={url}>
+                  <Image src={image} alt={``} width={148} height={44} />
+                </S.ImageLink>
+              ))}
+            </S.List>
+          </S.Links>
         </S.Caption>
       </Container>
       <S.Background>
@@ -51,7 +55,7 @@ const Hero = ({ data }: HeroProps) => {
           alt={data.title}
           sizes="100vw"
           style={{
-            objectPosition: 'center',
+            objectPosition: 'right',
             objectFit: 'cover'
           }}
           fill
